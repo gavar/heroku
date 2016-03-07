46045fa61d1a2b25a039f3d5e45b9ce0fdd5c5a3
Max Stankevich <m.stankevicsh@gmail.com>
2016-03-07 05:32:49 +0200

    FIX: .env settings

--- private/notes-sql.js
+++ private/notes-sql.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PWD,
-    database: process.env.MYSQL_SCHEME
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_BASE
 });
 
 connection.connect();
